511cb725d9448de9e16ffcc9d8a0b8bdad6e834e
Ivano Malavolta <ivanomalavolta@gmail.com>
2020-04-30 19:17:43 +0200

    refactor all crawling code into crawler.py

---
+++ code/crawler.py
@@ -0,0 +1,44 @@
+import time
+import json
+import sys
+import os
+import time
+from google_play_scraper import app
+import ssl
+import configuration as c
+import apk_downloader
+
+def download_apk(app_to_download, apk_path):
+    print('Downloading the APK of: ' + app_to_download)
+    return apk_downloader.download_apk(app_to_download, apk_path)
+    
+
+def get_gp_metadata(app_to_scrape):
+    ssl._create_default_https_context = ssl._create_unverified_context
+    app_metadata = app(app_to_scrape['id'], lang=app_to_scrape['store_country'], country=app_to_scrape['store_country'])
+    return app_metadata
+
+def crawl_data(app):
+    # Download Google Play metadata
+    app_metadata = get_gp_metadata(app)
+    app_latest_version = app_metadata['version']
+    app_suffix_path = app['id'] + c.app_version_separator + app_latest_version
+
+    # Save the metadata if it is new
+    metadata_path = c.data_path + app_suffix_path + c.app_version_separator + 'metadata.json'
+    reviews_path = c.data_path + app_suffix_path + c.app_version_separator + 'reviews.json'
+    if(not os.path.exists(metadata_path)):
+        c.save(metadata_path, app_metadata)
+    
+    # Download the APK if it is new
+    apk_path = c.apks_path + app_suffix_path + '.apk'
+    if(not os.path.exists(apk_path)):
+        if(not download_apk(app['id'], apk_path)):
+            print('Error while downloading the following app, try to download it manually: ' + app['id'])
+            exit()
+        elif(not apk_downloader.verify_apk(app['id'], apk_path, app_suffix_path)):
+            print('The downloaded APK is not well formed: ' + apk_path)
+            exit()
+    
+    app['latest_crawled_version'] = app_latest_version
+    app['latest_crawl'] = int(time.time())
